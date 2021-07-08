9d3ca27bba667679902264e5796a09ad7f5e07e1
Shivansh Potdar <loldude482@gmail.com>
2021-07-08 19:50:57 +0530

    pauseImplementation:nextMakeModular

--- src/main.rs
+++ src/main.rs
@@ -91,5 +91,23 @@ fn main() -> Result<(), das_grid::GridErr>{
     }
     println!("{}", liv_n);
 
+    //pause code
+    pause();
+
     Ok(())
+}
+
+use std::io;
+use std::io::prelude::*;
+
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
 }
